383a3586662e7143fbb351adffcc6ef23009369e
Shiraishi-Shodai <siranosuke1227@gmail.com>
2023-10-10 17:46:15 +0900

    コントローラー経由でテキストフィールドから値を受け取り、デバッグプリントする

--- lib/calculation.dart
+++ lib/calculation.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Calculaton extends StatelessWidget {
   const Calculaton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // 入力した値を管理するコントローラー
+    final num1Controller = TextEditingController();
+    final num2Controller = TextEditingController();
+
+    // 入力する値1
+    final num1 = TextField(
+        controller: num1Controller,
+        keyboardType: TextInputType.number,
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        enabled: true,
+        decoration: const InputDecoration(
+          icon: Icon(Icons.face),
+          hintText: '数字を入力してください',
+          labelText: '数字 * ',
+        ));
+    // onChanged: ((value) => print(value)));
+
+    // 入力する値2
+    final num2 = TextField(
+        controller: num2Controller,
+        keyboardType: TextInputType.number,
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        decoration: const InputDecoration(
+          icon: Icon(Icons.face),
+          labelText: '数字 * ',
+        ));
+    // onChanged: ((value) => print(value)));
+
+    final btn = ElevatedButton(
+      onPressed: () {
+        debugPrint('num1の値: ${num1Controller.text}');
+        debugPrint('num2の値: ${num2Controller.text}');
+      },
+      child: Text("表示する"),
+    );
+
+    final rowTest = Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [num1, num2, btn]);
+
     return Container(
-      width: 200,
-      height: 200,
-      child: TextField(onChanged: ((value) => print(value))),
-      alignment: Alignment.bottomCenter,
+      width: 500,
+      height: 600,
+      child: rowTest,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,56 +4,54 @@ import 'package:flutter/material.dart';
 void main() {
   final calc = Calculaton();
 
-  int plus(int a, int b) {
-    print(a + b);
-    return a + b;
-  }
+//   int plus(int a, int b) {
+//     print(a + b);
+//     return a + b;
+//   };
 
-  ;
+//   int minus(int a, int b) => a - b;
+//   int multiplication(int a, int b) => a * b;
+//   double division(double a, double b) => a / b;
 
-  int minus(int a, int b) => a - b;
-  int multiplication(int a, int b) => a * b;
-  double division(double a, double b) => a / b;
+// // 色 Colors.pink,Colors.orangeAccent[400],Colors.greenAccent[400],Colors.blue
+//   makeButton(String text, Color color, Function funType) {
+//     final btn = ElevatedButton(
+//         onPressed: () => funType(10, 5),
+//         child: Text("$text"),
+//         style: ElevatedButton.styleFrom(
+//             backgroundColor: color,
+//             side: BorderSide(color: Colors.black87),
+//             fixedSize: Size(120, 120),
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(70),
+//             )));
+//     return btn;
+//   }
 
-// 色 Colors.pink,Colors.orangeAccent[400],Colors.greenAccent[400],Colors.blue
-  makeButton(String text, Color color, Function funType) {
-    final btn = ElevatedButton(
-        onPressed: () => funType(10, 5),
-        child: Text("$text"),
-        style: ElevatedButton.styleFrom(
-            backgroundColor: color,
-            side: BorderSide(color: Colors.black87),
-            fixedSize: Size(120, 120),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(70),
-            )));
-    return btn;
-  }
+//   // それぞれのボタンを生成
+//   final plusBtn = makeButton("足し算", Colors.pink, plus);
+//   final minusBtn = makeButton("引き算", Colors.orange, minus);
+//   final multiplicationBtn =
+//       makeButton("掛け算", Colors.green, multiplication);
+//   final divisionBtn = makeButton("割り算", Colors.blue, division);
 
-  // それぞれのボタンを生成
-  final plusBtn = makeButton("足し算", Colors.pink, plus);
-  final minusBtn = makeButton("引き算", Colors.orange, minus);
-  final multiplicationBtn =
-      makeButton("掛け算", Colors.green, multiplication);
-  final divisionBtn = makeButton("割り算", Colors.blue, division);
+//   final btnRow = Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceAround,
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: [plusBtn, minusBtn, multiplicationBtn, divisionBtn]);
 
-  final btnRow = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [plusBtn, minusBtn, multiplicationBtn, divisionBtn]);
+//   final btnCon = Container(
+//       child: btnRow,
+//       alignment: Alignment.topCenter,
+//       margin: EdgeInsets.only(top: 150));
 
-  final btnCon = Container(
-      child: btnRow,
-      alignment: Alignment.topCenter,
-      margin: EdgeInsets.only(top: 150));
+//   final textCon = Container(
+//       alignment: Alignment.center,
+//       margin: EdgeInsets.only(top: 70),
+//       child: Text("数字を入力してください", style: TextStyle(fontSize: 24)));
 
-  final textCon = Container(
-      alignment: Alignment.center,
-      margin: EdgeInsets.only(top: 70),
-      child: Text("数字を入力してください", style: TextStyle(fontSize: 24)));
-
-  final col = Column(children: [btnCon, textCon, calc]);
-  final app = MaterialApp(home: Scaffold(body: Center(child: col)));
+  // final col = Column(children: [btnCon, textCon, calc]);
+  final app = MaterialApp(home: Scaffold(body: Center(child: calc)));
 
   runApp(app);
 }
